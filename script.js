1459391b648f8b86a427538ccefd64afe649554f
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-16 12:10:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 const makeChange = (c) => {
   // your name here
-	const coin={
-	     "q":0,
-		 "d":0,
-		 "n":0,
-		 "p"=0
-	};
-	   while(c>0)
-		{
-		  if(c>=25)
-		  {
-			coin.q= math.floor(c/25);
-			  c=c%25
-		  }
-			else if(c>=10)
-		  {
-			coin.d= math.floor(c/10);
-			  c=c%10
-		  }
-			else if(c>=5)
-		  {
-			coin.n= math.floor(c/5);
-			  c=c%5
-		  }
-			else
-		  {
-			coin.p= math.floor(c/1);
-			  c=c%1
-		  }
-		}
-	return coin;
+  const coin = {
+    "q": 0,  // Quarters
+    "d": 0,  // Dimes
+    "n": 0,  // Nickels
+    "p": 0   // Pennies
+  };
+
+  c = Number(c);  // Convert input to number
+
+  while (c > 0) {
+    if (c >= 25) {
+      coin.q = Math.floor(c / 25);
+      c = c % 25;
+    } else if (c >= 10) {
+      coin.d = Math.floor(c / 10);
+      c = c % 10;
+    } else if (c >= 5) {
+      coin.n = Math.floor(c / 5);
+      c = c % 5;
+    } else {
+      coin.p = Math.floor(c / 1);
+      c = c % 1;
+    }
+  }
+  return coin;
 };
 
-// Do not the change the code belo
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
+
