a971da49de8e8cf20b16b33054cd58fa7591b772
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-16 12:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const makeChange = (c) => {
   // your name here
+	const coin={
+	     "q":0,
+		 "d":0,
+		 "n":0,
+		 "p"=0
+	};
+	   while(c>0)
+		{
+		  if(c>=25)
+		  {
+			coin.q= math.floor(c/25);
+			  c=c%25
+		  }
+			else if(c>=10)
+		  {
+			coin.d= math.floor(c/10);
+			  c=c%10
+		  }
+			else if(c>=5)
+		  {
+			coin.n= math.floor(c/5);
+			  c=c%5
+		  }
+			else
+		  {
+			coin.p= math.floor(c/1);
+			  c=c%1
+		  }
+		}
+	return coin;
 };
 
-// Do not the change the code below
+// Do not the change the code belo
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
